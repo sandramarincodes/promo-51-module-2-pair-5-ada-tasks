501e4122598f0f2289d8a1eec0c97fa96d4df679
sandramarincodes <smarinfernan@gmail.com>
2025-05-06 21:00:35 +0200

    Update Adatask día 1

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const lista = document.querySelector(".js_list");
+
+const tasks = [
+  { name: "Recoger setas en el campo", completed: true, id: 1 },
+  { name: "Comprar pilas", completed: true, id: 2 },
+  { name: "Poner una lavadora de blancos", completed: true, id: 3 },
+  {
+    name: "Aprender cómo se realizan las peticiones al servidor en JavaScript",
+    completed: false,
+    id: 4,
+  },
+];
+
+for (const task of tasks) {
+  // pintar la tarea en la lista
+  lista.innerHTML += `<li>${task.name}</li>`;
+}
