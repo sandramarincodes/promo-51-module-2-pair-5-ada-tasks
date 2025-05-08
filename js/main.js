138c3c896b1dcb363e4fcfc34cf6cdf4d2d31979
MontseMoran <m-moran@hotmail.es>
2025-05-08 21:36:50 +0200

    Pair jueves 8 de mayo

--- js/main.js
+++ js/main.js
@@ -11,15 +11,31 @@ const tasks = [
   },
 ];
 
-
 for (const task of tasks) {
-  // pintar la tarea en la lista
-  //lista.innerHTML += `<li>${task.name}</li>`;//este solo duplica la lista sin tachar
   if (task.completed === true) {
-    //lista.innerHTML += `<li>${task.name.classList.add("completed")}</li>`; //pendiente preguntar Nuria classList no se usa en strings
-    lista.innerHTML += `<li class="completed">${task.name}</li>`;
-  } else{
-    lista.innerHTML += `<li>${task.name}</li>`;
+    lista.innerHTML += `<li class="completed"><input id=${task.id} type="checkbox">${task.name}</li>`;
+  } else {
+    lista.innerHTML += `<li><input id=${task.id} type="checkbox">${task.name}</li>`;
   }
 }
+function pintTasks() {
+  lista.innerHTML = ""; // Limpiamos la lista para volver a pintarla
 
+  for (const task of tasks) {
+    if (task.completed === true) {
+      lista.innerHTML += `<li class="completed"><input id=${task.id} type="checkbox">${task.name}</li>`;
+    } else {
+      lista.innerHTML += `<li><input id=${task.id} type="checkbox">${task.name}</li>`;
+    }
+  }
+}
+const handleClickList = (event) => {
+  const taskId = parseInt(event.target.id); // Obtengo el id del checkbox clickado por la usuaria
+  if (!taskId) return; // Si no ha pulsado en el checkbox, no queremos hacer nada y salimos de la función
+
+  const clickedTask = tasks.find((task) => task.id === taskId); // Busca la tarea que tenga el id `taskId` en el array `tasks`
+  task.completed = !clickedTask.completed; // task.completed = // Una vez que has obtenido la tarea, actualiza la propiedad `completed`
+  pintTasks(); // Pinta de nuevo las tareas en el html
+};
+
+list.addEventListener("click", handleClickList);
