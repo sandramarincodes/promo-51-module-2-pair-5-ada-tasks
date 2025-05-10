eaa462987cc3d526971fc62512fc35a890a2b049
SeiCp <seycuartero@gmail.com>
2025-05-10 15:00:44 +0200

    Changes

--- js/main.js
+++ js/main.js
@@ -2,8 +2,27 @@
 const lista = document.querySelector(".js_list");
 const botonFiltrar = document.querySelector(".js_right-block-btn");
 const inputFiltrar = document.querySelector(".js_input-right");
+const parrafoTareas = document.querySelector(".js_parrafo")
 
-const tasks = [
+//Crea variables para almacenar la información del usuario de github y la url del endpoint:
+const GITHUB_USER = "montsemoran";
+const SERVER_URL = `https://dev.adalab.es/api/todo/${GITHUB_USER}`;
+
+let tasks = [];
+
+fetch(SERVER_URL)
+  .then(response => response.json())
+  .then(data => {
+    tasks = data.results;
+    console.log (data);
+
+    pintTasks(tasks);
+
+  });
+
+
+
+/*let tasks = [
   { name: "Recoger setas en el campo", completed: true, id: 1 },
   { name: "Comprar pilas", completed: true, id: 2 },
   { name: "Poner una lavadora de blancos", completed: true, id: 3 },
@@ -12,7 +31,7 @@ const tasks = [
     completed: false,
     id: 4,
   },
-];
+];*/
 
 for (const task of tasks) {
   if (task.completed === true) {
@@ -22,11 +41,18 @@ for (const task of tasks) {
   }
 }
 
-// Funciones
-function pintTasks(arrayDeTareas = tasks) {
-  lista.innerHTML = ""; // Limpiamos la lista para volver a pintarla
 
-  for (const task of arrayDeTareas) {
+// Funciones
+function pintTasks(tasks) {
+
+  let tareasTotales = tasks.length
+  let tareasCompletadas = tasks.filter(task => task.completed).length;
+  let tareasPorRealizar = tareasTotales - tareasCompletadas
+
+  lista.innerHTML = ""; // Limpiamos la lista para volver a pintarla
+  parrafoTareas.innerHTML = `Tienes ${tareasTotales} tareas. ${tareasCompletadas} completadas y ${tareasPorRealizar} por realizar`
+
+  for (const task of tasks) {
     if (task.completed === true) {
       lista.innerHTML += `<li class="completed"><input id=${task.id} type="checkbox" checked>${task.name}</li>`;
     } else {
@@ -34,13 +60,14 @@ function pintTasks(arrayDeTareas = tasks) {
     }
   }
 }
+
 const handleClickList = (event) => {
   const taskId = parseInt(event.target.id); // Obtengo el id del checkbox clickado por la usuaria
   if (!taskId) return; // Si no ha pulsado en el checkbox, no queremos hacer nada y salimos de la función
 
   const clickedTask = tasks.find((task) => task.id === taskId); // Busca la tarea que tenga el id `taskId` en el array `tasks`
   clickedTask.completed = !clickedTask.completed; // pendiente Nuria //        // task.completed OR clickedTask?  Es clicked task// Una vez que has obtenido la tarea, actualiza la propiedad `completed`
-  pintTasks(); // Pinta de nuevo las tareas en el html
+  pintTasks(tasks); // Pinta de nuevo las tareas en el html
 };
 
 const handleClickBotonFiltrar = (event) => {
